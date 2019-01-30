1af3db8dadb60bece31c38ee30b1db05288d8270
Nimmala <mrudula.nimmala@capgemini.com>
2019-01-30 10:04:26 +0530

    adds mmbankapp zuulgateway

---
+++ src/test/java/com/moneymoney/web/moneymoneyzuulgateway/MoneymoneyZuulgatewayApplicationTests.java
@@ -0,0 +1,17 @@
+package com.moneymoney.web.moneymoneyzuulgateway;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MoneymoneyZuulgatewayApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
